2ab14cf0b3cf4c219fcbc2f5fbc9338fe932662f
Lobsang Monlam <monlamteche2020@gmail.com>
2024-05-07 19:28:56 +0530

    chang the mysql

--- includes/connect.php
+++ includes/connect.php
@@ -1,27 +1,12 @@
 <?php
-// Database connection parameters
-$dsn = 'mysql:host=localhost;dbname=cta_nekor_data';
-$username = 'root';
-$password = 'root';
-
-try {
-    // Create a new PDO instance
-    $db = new PDO($dsn, $username, $password);
-
-    // Set PDO to throw exceptions on error
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
-} catch (PDOException $e) {
-    // Handle any errors that occur during database connection or operations
-    echo 'Connection failed: ' . $e->getMessage();
-}
-
-
-//  $databasePath ='data/cta_nekor_data.db';
+// // Database connection parameters
+// $dsn = 'mysql:host=localhost;dbname=cta_nekor_data';
+// $username = 'root';
+// $password = 'root';
 
 // try {
-//     // Create a new PDO instance for SQLite
-//     $db = new PDO('sqlite:' . $databasePath);
+//     // Create a new PDO instance
+//     $db = new PDO($dsn, $username, $password);
 
 //     // Set PDO to throw exceptions on error
 //     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -30,3 +15,19 @@ try {
 //     // Handle any errors that occur during database connection or operations
 //     echo 'Connection failed: ' . $e->getMessage();
 // }
+
+
+ $databasePath ='data/cta_nekor_data.db';
+
+try {
+    // Create a new PDO instance for SQLite
+    $db = new PDO('sqlite:' . $databasePath);
+
+    // Set PDO to throw exceptions on error
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+} catch (PDOException $e) {
+    // Handle any errors that occur during database connection or operations
+    echo 'Connection failed: ' . $e->getMessage();
+}
+?>
