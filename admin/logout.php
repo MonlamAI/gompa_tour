2d2dc0281ef7b92af72c5b55c0c7d70cecffd312
tenkus47 <tenkus47@gmail.com>
2024-05-17 11:15:28 +0530

    update

--- admin/logout.php
+++ admin/logout.php
@@ -3,9 +3,11 @@ session_start();
 $uid = $_SESSION['id'];
 session_destroy();
 require_once ('../includes/connect.php');
-// redirect user to login page
-$domain = $_SERVER['HTTP_HOST'];
+// Construct the logout URL without '/admin'
 $protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http";
-$currentDir = $protocol . "://" . str_replace("/admin", "", $domain) . "/logout.php";
-header("location: " . $currentDir);
+$domain = $_SERVER['HTTP_HOST'];
+$currentDir = $protocol . "://" . $domain . "/logout.php";
+// Redirect to the logout page
+header("Location: " . $currentDir);
+exit;
 ?>
