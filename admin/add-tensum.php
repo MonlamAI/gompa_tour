4b5e1c2e568e7486b81b6959b5827d3562faab21
Lobsang Monlam <monlamteche2020@gmail.com>
2024-05-17 15:59:05 +0530

    change sessions

--- admin/add-tensum.php
+++ admin/add-tensum.php
@@ -9,7 +9,7 @@ ini_set('display_errors', 1);
 
 require_once ('../includes/connect.php');
 include ('includes/check-login.php');
-//include ('includes/check-subscriber.php');
+include ('includes/check-subscriber.php');
 
 require_once ('../includes/s3_functions.php');
 
