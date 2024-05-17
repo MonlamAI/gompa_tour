f83cfa70a1e5b3aac8519eba203e4d4ce4e29df6
tenkus47 <tenkus47@gmail.com>
2024-05-17 14:51:03 +0530

    update

--- admin/includes/check-login.php
+++ admin/includes/check-login.php
@@ -1,14 +1,20 @@
 <?php
 session_start();
 
-if (isset($_SESSION['login']) & ($_SESSION['login'] == true)) {
-	if (isset($_SESSION['id']) & !empty($_SESSION['id'])) {
-		if (isset($_SESSION['last_login']) & !empty($_SESSION['last_login'])) {
+if (isset($_SESSION['login']) && $_SESSION['login'] == true) {
+	if (isset($_SESSION['id']) && !empty($_SESSION['id'])) {
+		if (isset($_SESSION['last_login']) && !empty($_SESSION['last_login'])) {
+			// Session values are set, continue with your logic
+		} else {
+			// Handle case when 'last_login' is not set
 		}
+	} else {
+		// Handle case when 'id' is not set
 	}
 } else {
 	// redirect user to login page
-	header('location:login.php');
+	header('location: login.php');
+	exit; // Important to stop further execution after redirecting
 }
 
 
@@ -21,7 +27,7 @@ if (isset($_SESSION['id']) & !empty($_SESSION['id'])) {
 	if ($user['role'] == 'subscriber') {
 		//echo "role: subscriber";
 		// redirect to Blog Home page
-		header("location: admin/dashboard.php");
+		header("location: index.php");
 	}
 }
 
