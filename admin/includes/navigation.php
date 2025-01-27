d474d1626426fe2d8006521d64b19e8ab254588f
Monlam IT <mlbodyig@gmail.com>
2025-01-27 18:16:52 +0530

    Update navigation.php

--- admin/includes/navigation.php
+++ admin/includes/navigation.php
@@ -100,6 +100,18 @@
                     <!-- /.nav-second-level -->
                 </li>
                 <li>
+                    <a href="#"><i class="fa fa-institution"></i> གནས་ཆེན།<span class="fa arrow"></span></a>
+                    <ul class="nav nav-second-level">
+                        <li>
+                            <a href="add-organization.php">གནས་ཆེན་གསར་སྣོན།</a>
+                        </li>
+                        <li>
+                            <a href="view-organization.php">གནས་ཆེན་རེའུ་མིག</a>
+                        </li>
+                    </ul>
+                    <!-- /.nav-second-level -->
+                </li>
+                <li>
                     <a href="#"><i class='fa fa-calendar-check-o'></i> དུས་ཆེན།<span class="fa arrow"></span></a>
                     <ul class="nav nav-second-level">
                         <li>
